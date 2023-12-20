272f4245839b8fb9d716dbba2bb8f5083ac470f6
James <fairbanksj@ufl.edu>
2023-12-20 12:03:53 -0500

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -13,4 +13,4 @@ echo "Running Tests..."
 julia --project -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia --project=docs -e 'using Pkg; Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
+julia --project=docs -e 'using Pkg; Pkg.develop(PackageSpec(path=pwd())); Pkg.status(); Pkg.instantiate(); include("docs/make.jl")'
