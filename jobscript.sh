b117d1cd68840c0c2fbe91d9fc3c1a2292c3cb8d
James <fairbanksj@ufl.edu>
2023-12-20 11:40:31 -0500

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=daemon_job_test    # Job name
+#SBATCH --job-name=diagrammatic_equations_CI    # Job name
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=cuffaro.m@ufl.edu # Where to send mail	
 #SBATCH --ntasks=1                    # Run on a single CPU
 #SBATCH --mem=8gb                     # Job memory request
-#SBATCH --time=00:05:00               # Time limit hrs:min:sec
-#SBATCH --output=daemon_test_%j.log   # Standard output and error log
+#SBATCH --time=00:15:00               # Time limit hrs:min:sec
 pwd; hostname; date
 
 module load julia
@@ -15,7 +14,7 @@ echo "Installing Packages..."
 julia --project=docs/ -e 'using Pkg; Pkg.status()'
 
 echo "Running Tests..."
-julia --project -e 'using Pkg; Pkg.status; Pkg.test()'
+julia --project -e 'using Pkg; Pkg.status(); Pkg.test()'
 
 echo "Building Documentation..."
-julia --project=docs/ -e 'using Pkg; Pkg.instantiate(); include("docs/make.jl")'
+julia --project=docs/ -e 'using Pkg; Pkg.status(); include("docs/make.jl")'
