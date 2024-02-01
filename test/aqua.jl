b28cb7aff3fb98e9fde05a655c5fea3e1d5c3f0e
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-02-01 15:52:20 -0600

    add ambiguities comment

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,6 +1,5 @@
 using Aqua, DiagrammaticEquations
 @testset "Code quality (Aqua.jl)" begin
-    Aqua.test_all(DiagrammaticEquations,
-        ambiguities=false,
-    )
+    # TODO: fix ambiguities
+    Aqua.test_all(DiagrammaticEquations, ambiguities=false)
 end
