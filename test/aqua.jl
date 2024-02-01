bd113bdb928f3177009c80458c854a8c56488cc5
Lilith Orion Hafner <lilithhafner@gmail.com>
2024-02-01 15:46:27 -0600

    fix mising dep compat

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,7 +1,6 @@
 using Aqua, DiagrammaticEquations
 @testset "Code quality (Aqua.jl)" begin
     Aqua.test_all(DiagrammaticEquations,
-        deps_compat=false,
         ambiguities=false,
         undefined_exports=false
     )
