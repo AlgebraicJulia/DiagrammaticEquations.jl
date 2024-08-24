0b52472f68ca162adf927bac5f047dcfaea33608
Luke Morris <lukelukemorrismorris@gmail.com>
2024-08-24 14:51:24 -0400

    Clean by caching hyperedge order

--- src/graph_traversal.jl
+++ src/graph_traversal.jl
@@ -3,35 +3,33 @@ using ACSets
 
 export TraversalNode, topological_sort_edges, number_of_ops, retrieve_name
 
-abstract type TraversalNode end
-
-struct Op1TravNode <: TraversalNode
+struct TraversalNode{T}
   index::Int
-end
-struct Op2TravNode <: TraversalNode
-  index::Int
-end
-struct ΣTravNode <: TraversalNode
-  index::Int
+  name::T
+  dom::AbstractVector
+  cod::Int
 end
 
-# TODO: If the name of an operator is cached, only one definition is necessary.
-retrieve_name(d, tsr::Op1TravNode) = d[tsr.index, :op1]
-retrieve_name(d, tsr::Op2TravNode) = d[tsr.index, :op2]
-retrieve_name(d, tsr::ΣTravNode) = :+
+TraversalNode(i, d::SummationDecapode, ::Val{:Op1}) =
+  TraversalNode{Symbol}(i, d[i,:op1], [d[i,:src]], d[i,:tgt])
 
-# TODO: If the codomain of an operator is cached, only one definition is necessary.
-# Induce a topological ordering of operations from one of variables.
-edge2cost(d, tsv, tsr::Op1TravNode) = tsv[d[tsr.index,:src]]
-edge2cost(d, tsv, tsr::Op2TravNode) = max(tsv[d[tsr.index,:proj1]], tsv[d[tsr.index,:proj1]])
-edge2cost(d, tsv, tsr::ΣTravNode) = maximum(tsv[d[incident(d,tsr.index,:summation),:summand]])
+TraversalNode(i, d::SummationDecapode, ::Val{:Op2}) =
+  TraversalNode{Symbol}(i, d[i,:op2], [d[i,:proj1],d[i,:proj2]], d[i,:res])
+
+TraversalNode(i, d::SummationDecapode, ::Val{:Σ}) =
+  TraversalNode{Symbol}(i, :+, d[incident(d,i,:summation),:summand], d[i,:sum])
+
+retrieve_name(d::SummationDecapode, tsr::TraversalNode) = tsr.name
+
+# Induce a topological ordering of operations from a topological ordering of variables.
+# Taking Vᵢ(cod(e)ᵢ) like so is a structure preserving map.
+edge2cost(tsv, tsr::TraversalNode) = maximum(tsv[tsr.dom])
 
 number_of_ops(d::SummationDecapode) = nparts(d, :Op1) + nparts(d, :Op2) + nparts(d, :Σ)
 
 start_nodes(d::SummationDecapode) = vcat(infer_states(d), incident(d, :Literal, :type))
 
 
-# TODO: Domain-Codomain pointers could be upstreamed into TraversalNode subtypes. Then this can be cleaned up with multiple dispatch. This would explicitly cache calls to incident.
 # TODO: This could be Catlab'd. Hypergraph category? Migration to a DWD?
 """    function hyper_edge_list(d::SummationDecapode)
 
@@ -43,16 +41,9 @@ Interpret a:
   - summation as a hyperedge of order (|summands|,1) .
 """
 function hyper_edge_list(d::SummationDecapode)
-  reduce(vcat, [
-    map(parts(d,:Op1)) do e
-      ([d[e,:src]], d[e,:tgt])
-    end,
-    map(parts(d,:Op2)) do e
-      ([d[e,:proj1],d[e,:proj2]], d[e,:res])
-    end,
-    map(parts(d,:Σ)) do e
-      (d[incident(d,e,:summation),:summand], d[e,:sum])
-    end])
+  [map(e -> TraversalNode(e, d, Val(:Op1)), parts(d, :Op1))...,
+   map(e -> TraversalNode(e, d, Val(:Op2)), parts(d, :Op2))...,
+   map(e -> TraversalNode(e, d, Val(:Σ  )), parts(d, :Σ  ))...]
 end
 
 """    function floyd_warshall(d::SummationDecapode)
@@ -68,7 +59,7 @@ function floyd_warshall(d::SummationDecapode)
   V = nparts(d, :Var)
   dist = fill(Inf, (V, V))
   foreach(hyper_edge_list(d)) do e
-    dist[e...] .= 1
+    dist[(e.dom), e.cod] .= 1
   end
   for v in 1:V
     dist[v,v] = 0
@@ -99,13 +90,14 @@ function topological_sort_verts(d::SummationDecapode)
   end
 end
 
+# TODO: Add in-place version for sorting a given hyperedge list.
 """    function topological_sort_edges(d::SummationDecapode)
 
 Topologically sort the edges in a Decapode.
 """
 function topological_sort_edges(d::SummationDecapode)
   tsv = topological_sort_verts(d)
-  op_order = [Op1TravNode.(parts(d,:Op1))..., Op2TravNode.(parts(d,:Op2))..., ΣTravNode.(parts(d,:Σ))...]
-  sort(op_order, by = x -> edge2cost(d,tsv,x))
+  op_order = hyper_edge_list(d)
+  sort(op_order, by = x -> edge2cost(tsv,x))
 end
 
