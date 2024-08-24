d46cf86a61b8de7821cdc3dd4628c01328f69ade
Luke Morris <lukelukemorrismorris@gmail.com>
2024-08-24 12:30:37 -0400

    Add docstrings and use multiple dispatch

--- src/graph_traversal.jl
+++ src/graph_traversal.jl
@@ -3,16 +3,39 @@ using ACSets
 
 export TraversalNode, topological_sort_edges, number_of_ops, retrieve_name
 
-struct TraversalNode{T}
+abstract type TraversalNode end
+
+struct Op1TravNode <: TraversalNode
   index::Int
-  name::T
 end
+struct Op2TravNode <: TraversalNode
+  index::Int
+end
+struct ΣTravNode <: TraversalNode
+  index::Int
+end
+
+retrieve_name(d, tsr::Op1TravNode) = d[tsr.index, :op1]
+retrieve_name(d, tsr::Op2TravNode) = d[tsr.index, :op2]
+retrieve_name(d, tsr::ΣTravNode) = :+
+
+# Induce a topological ordering of operations from one of variables.
+edge2cost(d, tsv, tsr::Op1TravNode) = tsv[d[tsr.index,:src]]
+edge2cost(d, tsv, tsr::Op2TravNode) = max(tsv[d[tsr.index,:proj1]], tsv[d[tsr.index,:proj1]])
+edge2cost(d, tsv, tsr::ΣTravNode) = maximum(tsv[d[incident(d,tsr.index,:summation),:summand]])
 
 number_of_ops(d::SummationDecapode) = nparts(d, :Op1) + nparts(d, :Op2) + nparts(d, :Σ)
 
 start_nodes(d::SummationDecapode) = vcat(infer_states(d), incident(d, :Literal, :type))
 
-#https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm#Pseudocode
+"""    function floyd_warshall(d::SummationDecapode)
+
+Return a |variable| × |variable| matrix of shortest paths via the Floyd-Warshall algorithm.
+
+Taking the maximum of the non-infinite short paths from state variables induces a topological ordering.
+
+https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm
+"""
 function floyd_warshall(d::SummationDecapode)
   # Init dists.
   V = nparts(d, :Var)
@@ -43,36 +66,26 @@ function floyd_warshall(d::SummationDecapode)
   dist
 end
 
+"""    function topological_sort_verts(d::SummationDecapode)
+
+Topologically sort the variables in a Decapode.
+
+The vector returned by this function maps each vertex to the order that it would be traversed in a topological sort traversal. If you want a list of vertices in the order of traversal, call `sortperm` on the output.
+"""
 function topological_sort_verts(d::SummationDecapode)
   m = floyd_warshall(d)
   map(parts(d,:Var)) do v
     maximum(filter(!isinf, m[start_nodes(d),v]))
   end
-  # Call sortperm for the vertex ordering.
 end
 
+"""    function topological_sort_edges(d::SummationDecapode)
+
+Topologically sort the edges in a Decapode.
+"""
 function topological_sort_edges(d::SummationDecapode)
   tsv = topological_sort_verts(d)
-  op_order = [TraversalNode.(parts(d,:Op1), :Op1)...,
-              TraversalNode.(parts(d,:Op2), :Op2)...,
-              TraversalNode.(parts(d,:Σ), :Σ)...]
-  function by(x)
-    @match x.name begin
-      :Op1 => tsv[d[x.index,:src]]
-      :Op2 => max(tsv[d[x.index,:proj1]], tsv[d[x.index,:proj1]])
-      :Σ => maximum(tsv[d[incident(d,x.index,:summation),:summand]])
-      _ => error("Unknown function type")
-    end
-  end
-  sort(op_order, by = by)
-end
-
-function retrieve_name(d::SummationDecapode, tsr::TraversalNode)
-  @match tsr.name begin
-    :Op1 => d[tsr.index, :op1]
-    :Op2 => d[tsr.index, :op2]
-    :Σ => :+
-    _ => error("$(tsr.name) is not a valid table for names")
-  end
+  op_order = [Op1TravNode.(parts(d,:Op1))..., Op2TravNode.(parts(d,:Op2))..., ΣTravNode.(parts(d,:Σ))...]
+  sort(op_order, by = x -> edge2cost(d,tsv,x))
 end
 
