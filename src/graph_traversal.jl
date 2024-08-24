db988a29e4110167ec8fed58fa87105d37de5717
Luke Morris <lukelukemorrismorris@gmail.com>
2024-08-24 14:12:32 -0400

    Explicitly create hyperedge list

--- src/graph_traversal.jl
+++ src/graph_traversal.jl
@@ -15,10 +15,12 @@ struct ΣTravNode <: TraversalNode
   index::Int
 end
 
+# TODO: If the name of an operator is cached, only one definition is necessary.
 retrieve_name(d, tsr::Op1TravNode) = d[tsr.index, :op1]
 retrieve_name(d, tsr::Op2TravNode) = d[tsr.index, :op2]
 retrieve_name(d, tsr::ΣTravNode) = :+
 
+# TODO: If the codomain of an operator is cached, only one definition is necessary.
 # Induce a topological ordering of operations from one of variables.
 edge2cost(d, tsv, tsr::Op1TravNode) = tsv[d[tsr.index,:src]]
 edge2cost(d, tsv, tsr::Op2TravNode) = max(tsv[d[tsr.index,:proj1]], tsv[d[tsr.index,:proj1]])
@@ -27,6 +29,31 @@ edge2cost(d, tsv, tsr::ΣTravNode) = maximum(tsv[d[incident(d,tsr.index,:summati
 number_of_ops(d::SummationDecapode) = nparts(d, :Op1) + nparts(d, :Op2) + nparts(d, :Σ)
 
 start_nodes(d::SummationDecapode) = vcat(infer_states(d), incident(d, :Literal, :type))
+
+
+# TODO: Domain-Codomain pointers could be upstreamed into TraversalNode subtypes. Then this can be cleaned up with multiple dispatch. This would explicitly cache calls to incident.
+# TODO: This could be Catlab'd. Hypergraph category? Migration to a DWD?
+"""    function hyper_edge_list(d::SummationDecapode)
+
+Represent a Decapode as a directed hyper-edge list.
+
+Interpret a:
+  - unary operation as a hyperedge of order (1,1) ,
+  - binary operation as a hyperedge of order (2,1) , and
+  - summation as a hyperedge of order (|summands|,1) .
+"""
+function hyper_edge_list(d::SummationDecapode)
+  reduce(vcat, [
+    map(parts(d,:Op1)) do e
+      ([d[e,:src]], d[e,:tgt])
+    end,
+    map(parts(d,:Op2)) do e
+      ([d[e,:proj1],d[e,:proj2]], d[e,:res])
+    end,
+    map(parts(d,:Σ)) do e
+      (d[incident(d,e,:summation),:summand], d[e,:sum])
+    end])
+end
 
 """    function floyd_warshall(d::SummationDecapode)
 
@@ -37,18 +64,11 @@ Taking the maximum of the non-infinite short paths from state variables induces 
 https://en.wikipedia.org/wiki/Floyd–Warshall_algorithm
 """
 function floyd_warshall(d::SummationDecapode)
-  # Init dists.
+  # Init dists
   V = nparts(d, :Var)
   dist = fill(Inf, (V, V))
-  foreach(parts(d,:Op1)) do e
-    dist[d[e,:src], d[e,:tgt]] = 1
-  end
-  foreach(parts(d,:Op2)) do e
-    dist[d[e,:proj1], d[e,:res]] = 1
-    dist[d[e,:proj2], d[e,:res]] = 1
-  end
-  foreach(parts(d,:Summand)) do e
-    dist[d[e,:summand], d[e,[:summation, :sum]]] = 1
+  foreach(hyper_edge_list(d)) do e
+    dist[e...] .= 1
   end
   for v in 1:V
     dist[v,v] = 0
