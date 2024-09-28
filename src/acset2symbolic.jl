fb4927c1952d446f8362461564598ec4400d6cca
Luke Morris <lukelukemorrismorris@gmail.com>
2024-09-28 14:13:13 -0400

    Pass indexed names and types directly

--- src/acset2symbolic.jl
+++ src/acset2symbolic.jl
@@ -9,14 +9,14 @@ const EQUALITY = (==)
 const SymEqSym = SymbolicEquation{Symbolic}
 
 function symbolics_lookup(d::SummationDecapode)
-  Dict{Symbol, BasicSymbolic}(map(parts(d, :Var)) do i
-    (d[i, :name], decavar_to_symbolics(d, i))
+  Dict{Symbol, BasicSymbolic}(map(d[:name],d[:type]) do name,type
+    (name, decavar_to_symbolics(d, name, type))
   end)
 end
 
-function decavar_to_symbolics(d::SummationDecapode, var_idx::Int; space = :I)
-  new_type = SymbolicUtils.symtype(Deca.DECQuantity, d[var_idx, :type], space)
-  SymbolicUtils.Sym{new_type}(d[var_idx, :name])
+function decavar_to_symbolics(d::SummationDecapode, var_name::Symbol, var_type::Symbol; space = :I)
+  new_type = SymbolicUtils.symtype(Deca.DECQuantity, var_type, space)
+  SymbolicUtils.Sym{new_type}(var_name)
 end
 
 function to_symbolics(d::SummationDecapode, symvar_lookup::Dict{Symbol, BasicSymbolic}, op_idx::Int, op_type::Symbol)
@@ -42,14 +42,13 @@ end
 # XXX SymbolicUtils.substitute swaps the order of multiplication.
 # e.g. ∂ₜ(G) == κ*u becomes ∂ₜ(G) == u*κ
 function merge_equations(d::SummationDecapode)
-  symexprs = to_symbolics(d)
-  eqn_lookup = Dict()
-  terminal_eqns = SymEqSym[]
+  eqn_lookup, terminal_eqns = Dict(), SymEqSym[]
 
-  foreach(symexprs) do x
-    push!(eqn_lookup, (x.lhs => SymbolicUtils.substitute(x.rhs, eqn_lookup)))
+  foreach(to_symbolics(d)) do x
+    sub = SymbolicUtils.substitute(x.rhs, eqn_lookup)
+    push!(eqn_lookup, (x.lhs => sub))
     if x.lhs.name in infer_terminal_names(d)
-      push!(terminal_eqns, SymEqSym(x.lhs, eqn_lookup[x.lhs]))
+      push!(terminal_eqns, SymEqSym(x.lhs, sub))
     end
   end
 
