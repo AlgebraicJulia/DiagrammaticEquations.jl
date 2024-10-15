77d7d18011140167223acf479c436e42ddf47594
Luke Morris <70283489+lukem12345@users.noreply.github.com>
2024-10-15 17:40:26 -0400

    Apply collages to intermediate variables

--- src/collages.jl
+++ src/collages.jl
@@ -7,24 +7,20 @@ end
 
 collate(c::Collage) = collate(c.src, c.tgt, c.uwd, c.symbols)
 
+# TODO: This is assuming only "restriction"-type morphisms.
+# TODO: Throw an error if the user tries to use a boundary value differential
+# form that is of a different type of the thing that we are applying the bound
+# to. i.e. Form1 but target is a Form0.
 """    function collate(equations, boundaries, uwd, symbols)
 
 Create a collage of two Decapodes that simulates with boundary conditions.
 ```
 """
 function collate(equations, boundaries, uwd, symbols)
-  # TODO: This is assuming only "restriction"-type morphisms.
-
   f = SummationDecapode{Any, Any, Symbol}()
-  # TODO: Double-check
   copy_parts!(f, equations, (:Var, :TVar, :Op1, :Op2, :Σ, :Summand))
-
-  # TODO: Throw an error if the user tries to use a boundary value differential
-  # form that is of a different type of the thing that we are applying the bound
-  # to. i.e. Form1 but target is a Form0.
-
-  # TODO: This sets restrictions as Op1s. They are actually Op2s. i.e. Use `bv`.
   for b in boxes(uwd)
+    # Set up pointers.
     ps = incident(uwd, b, :box)
     ev = first(ps)
     bv = last(ps)
@@ -33,15 +29,22 @@ function collate(equations, boundaries, uwd, symbols)
     en = symbols[en_key]
     bn = symbols[bn_key]
     var = only(incident(f, en, :name))
-    b_var = add_part!(f, :Var, type=f[var, :type], name=f[var, :name])
-    f[var, :name] = Symbol("r$(b)_" * string(f[var, :name]))
+    # Add a new variable and transfer the children of the original variable to it.
+    b_var = add_part!(f, :Var, type=f[var, :type], name=Symbol("r$(b)_" * string(f[var, :name])))
+    transfer_children!(f, var, b_var)
+    # Transfer ∂ₜ morphisms back, if any.
+    tangent_op1s = filter(x -> f[x, :op1]==:∂ₜ, incident(f, b_var, :src))
+    if !isempty(tangent_op1s)
+      f[only(tangent_op1s), :src] = var
+    end
+    # Transfer ∂ₜ morphisms forward, if any.
+    tangent_op1s = filter(x -> f[x, :op1]==:∂ₜ, incident(f, var, :tgt))
+    if !isempty(tangent_op1s)
+      f[tangent_op1s, :tgt] = b_var
+    end
+    # Insert the "masking" operation.
     s_var = add_part!(f, :Var, type=boundaries[only(incident(boundaries, bn, :name)), :type], name=bn)
-    add_part!(f, :Op2, proj1=b_var, proj2=s_var, res=var, op2=uwd[b, :name])
-
-    # Update tangent variable pointers, if any.
-    tangent_op1s = filter(x -> f[x, :op1]==:∂ₜ, incident(f, var, :src))
-    isempty(tangent_op1s) && continue
-    f[only(tangent_op1s), :src] = b_var
+    add_part!(f, :Op2, proj1=var, proj2=s_var, res=b_var, op2=uwd[b, :name])
   end
 
   f
