9e8ea4a2b3d66065d186bbf3de4d958cad870f1b
Christian Scaff <97995580+cscaff@users.noreply.github.com>
2025-02-03 11:28:17 -0500

    Created Testing Module for Decapode DSL PEG Parser.
    Co-authored-by: DZ03FL <DZ03FL@users.noreply.github.com>

--- src/parser.jl
+++ src/parser.jl
@@ -1,2 +1,15 @@
 @intertypes "decapodes.it" module decapodes end
 using .decapodes
+
+# Line := Statement & EOL
+# Statement := Judgement | Eq
+# Judgement := var::var
+# var := Symbol | Expr
+# Eq := term & "==" & term
+# Term := derivative | function | operation  
+# derivative = (∂ₜ | dt) Var
+# function = ident (args)
+# args = term | term, term
+# operation = term ((+|*) term)+
+# compose = ∘(args)(term)
+
---
+++ test/parser.jl
@@ -0,0 +1,7 @@
+using Test
+using Catlab
+using LinearAlgebra
+# using MLStyle
+using Base.Iterators
+
+using DiagrammaticEquations
