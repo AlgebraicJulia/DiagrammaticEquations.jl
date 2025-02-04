a8fa49010bf8a2cc1265dcadab9ec2b9ceab0406
Christian Scaff <97995580+cscaff@users.noreply.github.com>
2025-02-04 17:28:20 -0500

    Added Arg test to test derivative term argument

--- test/parser.jl
+++ test/parser.jl
@@ -7,10 +7,10 @@ using Base.Iterators
 using DiagrammaticEquations
 
 
-PEG.setdebug!(true)
+PEG.setdebug!(true) # To disable: PEG.setdebug!(false)
 
 @testset "Terms" begin
-    @test Term("∂ₜ(X)")[1] == Tan(DiagrammaticEquations.decapodes.Var(Symbol("X")))
+    @test Term("∂ₜ(X)")[1] == Tan(DiagrammaticEquations.decapodes.Var(Symbol("X"))) # Need to specify "DiagrammaticEquations.decapodes" b/c Catlab import also has "Var".
 end
 
 @testset "Derivatives" begin
@@ -25,4 +25,5 @@ end
 @testset "Args" begin
     @test Args("a")[1] == :a
     @test Args("a, b")[1] == [:a, :b]
+    @test Args("∂ₜ(X)")[1] == Tan(DiagrammaticEquations.decapodes.Var(Symbol("X")))
 end
